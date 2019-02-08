18149a4c54e806fa1829367d8c220cdf04051af2
Jdroetti <juliandroetti@gmail.com>
2019-02-07 22:41:40 -0800

    added segmented bar and some shitty text fields for new game

--- ExcaliburTns/src/app/home/home.component.ts
+++ ExcaliburTns/src/app/home/home.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from "@angular/core";
+import { SegmentedBar, SegmentedBarItem } from "tns-core-modules/ui/segmented-bar";
+import {
+    GestureEventData,
+    PanGestureEventData,
+    PinchGestureEventData,
+    RotationGestureEventData,
+    SwipeGestureEventData,
+    TouchGestureEventData} from "tns-core-modules/ui/gestures";
+import { EventData } from "tns-core-modules/data/observable";
+import { View } from "tns-core-modules/ui/core/view";
 
 @Component({
     selector: "Home",
@@ -7,8 +17,25 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
+    public items: Array<SegmentedBarItem>;
+    public selectedIndex = 0;
+
     constructor() {
-        // Use the component constructor to inject providers.
+        this.items = [];
+        let segment1 = <SegmentedBarItem>new SegmentedBarItem();
+        segment1.title = "New Game";
+        let segment2 = <SegmentedBarItem>new SegmentedBarItem();
+        segment2.title = "Set Lineup";
+        let segment3 = <SegmentedBarItem>new SegmentedBarItem();
+        segment3.title = "Settings"
+        this.items.push(segment1);
+        this.items.push(segment2);
+        this.items.push(segment3);
+    }
+
+    public onSelectedIndexChange(args) {
+        let segmetedBar = <SegmentedBar>args.object;
+        this.selectedIndex = segmetedBar.selectedIndex;
     }
 
     ngOnInit(): void {
